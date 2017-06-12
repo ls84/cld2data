7ba9fc43f3667ef013af2c572dd22f6a0e524a66
Liu Shuo <liu@shuo.email>
2017-06-12 11:28:51 +0800

    cli prints metadata

--- src/cli.js
+++ src/cli.js
@@ -9,15 +9,17 @@ let pdfFilePath = path.resolve(process.cwd(), argv._[0])
 let writeFolderPath = path.resolve(process.cwd(), argv._[1])
 let writeFilePath = `${writeFolderPath}/${pdfFilePath.split('/').pop().split('.')[0]}.json`
 
-process.stdout.write(`converting from ${pdfFilePath} to ${writeFilePath}`)
+//TODO: windows file path uses \ instead, regex will not match
+let datetime = /(\d\d)\/(\d\d\d\d)\.pdf/.exec(pdfFilePath)
+process.stdout.write(`${datetime[1]},${datetime[2]}`)
 rundownPDFParser(pdfFilePath)
 .then((json) => {
   fs.writeFileSync(writeFilePath, JSON.stringify(json, null, 2))
-  console.log(' done')
+  console.log(`,${json.meta},done`)
 })
 .catch((error) => {
   console.log(error)
 })
 
 // NOTE: batch processing
-// ls | xargs -I {} cld2data /input/folder /output/folder
+// ls | xargs -I {} cld2data {} /output/folder
