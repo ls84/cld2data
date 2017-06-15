15c9c54845e85297e0c51fc7357d6083dcbf2629
Liu Shuo <liu@shuo.email>
2017-06-15 10:18:46 +0800

    write out tsv on -f tsv flag

--- src/cli.js
+++ src/cli.js
@@ -22,6 +22,27 @@ switch (procedure) {
   case 'cleanRundownWithDuration':
     let jsonFilePath = path.resolve(process.cwd(), argv._[1])
     let cleanedJSON = cleanRundownWithDuration(jsonFilePath)
+    if (argv.f === 'tsv') {   
+      let tsv = ['date', 'program', 'index', 'format', 'text', 'startTime', 'endTime', 'duration'].join('\t') + '\n'
+      for (let key in cleanedJSON) {
+        if (key !== 'meta') {
+          let row = cleanedJSON[key]
+          let data = [
+            cleanedJSON.meta.date,
+            cleanedJSON.meta.program,
+            key,
+            row.format.join(),
+            row.text.join(),
+            row.startTime.toISOString(),
+            row.endTime.toISOString(),
+            row.duration / 1000
+          ]
+          tsv += data.join('\t')
+          tsv += '\n'
+        }
+      }
+      return process.stdout.write(tsv)
+    }
     process.stdout.write(JSON.stringify(cleanedJSON, null, 2))
 }
 
